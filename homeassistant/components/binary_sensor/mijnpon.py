bdad09811c3d378aa7db48148b6e797b08c13d1e
Bram Kragten <mail@bramkragten.nl>
2017-09-01 15:01:24 +0200

    Update mijnpon.py

--- homeassistant/components/binary_sensor/mijnpon.py
+++ homeassistant/components/binary_sensor/mijnpon.py
@@ -13,9 +13,9 @@ _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = ['mijnpon']
 
 SENSOR_TYPES = {
-    'ignition': ['Ignition', 1, 'power', 'mdi:power', 'mdi:power'],
-    'locked': ['Locked', 0, 'opening', 'mdi:lock', 'mdi:lock-open'],
-    'parking_brake': ['Parking brake', 0, 'safety', 'mdi:parking', 'mdi:parking']
+    'ignition': ['Ignition', True, 'power', 'mdi:power', 'mdi:power'],
+    'locked': ['Locked', False, 'opening', 'mdi:lock', 'mdi:lock-open'],
+    'parking_brake': ['Parking brake', False, 'safety', 'mdi:parking', 'mdi:parking']
 }
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -24,10 +24,11 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         return
 
     devs = list()
-    for vehicle in hass.data[DATA_MIJNPON].vehicles():
-        devs.append(MijnPonBinarySensor(vehicle.license_plate, 'ignition', vehicle))
-        devs.append(MijnPonBinarySensor(vehicle.license_plate, 'locked', vehicle))
-        devs.append(MijnPonBinarySensor(vehicle.license_plate, 'parking_brake', vehicle))
+    mijnPon = hass.data[DATA_MIJNPON]
+    for vehicle in mijnPon.vehicles():
+        devs.append(MijnPonBinarySensor(vehicle.license_plate, 'ignition', mijnPon, vehicle))
+        devs.append(MijnPonBinarySensor(vehicle.license_plate, 'locked', mijnPon, vehicle))
+        devs.append(MijnPonBinarySensor(vehicle.license_plate, 'parking_brake', mijnPon, vehicle))
 
     add_devices(devs, True)
 
@@ -35,11 +36,12 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class MijnPonBinarySensor(BinarySensorDevice):
     """A MijnPon sensor."""
 
-    def __init__(self, name, sensor_type, vehicle):
+    def __init__(self, name, sensor_type, mijnPon, vehicle):
         """Initialize sensors from the car."""
         self._name = name + ' ' + SENSOR_TYPES[sensor_type][0]
         self._type = sensor_type
         self._vehicle = vehicle
+        self._mijnPon = mijnPon
         self._state = None
         self._on_state = SENSOR_TYPES[sensor_type][1]
         self._icon_off = SENSOR_TYPES[sensor_type][3]
@@ -72,11 +74,15 @@ class MijnPonBinarySensor(BinarySensorDevice):
     def update(self):
         """Retrieve sensor data from the car."""
         if self._type == 'ignition':
-            self._state = self._vehicle.measureddata.get('Power').get('ValueDecimal')
+            self._state = self._vehicle.measureddata('Power')
+            if self._state == True:
+                self._mijnPon.cache_ttl = 30
+            else:
+                self._mijnPon.cache_ttl = 270
         elif self._type == 'locked':
-            self._state = self._vehicle.measureddata.get('VehicleLocked').get('ValueDecimal')
+            self._state = self._vehicle.measureddata('VehicleLocked')
         elif self._type == 'parking_brake':
-            self._state = self._vehicle.measureddata.get('ElectronicParkingBrake').get('ValueDecimal')
+            self._state = self._vehicle.measureddata('ElectronicParkingBrake')
         else:
             self._state = None
             _LOGGER.warning("Could not retrieve state from %s", self.name)
